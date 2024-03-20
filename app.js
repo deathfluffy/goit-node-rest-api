564ca205a4b82fe6b92f161e2d6ac1e14e7a1c04
deathfluffy <mrandreu454@gmail.com>
2024-03-20 18:28:32 +0200

    ++++

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ dotenv.config();
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"))
+
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,21 @@
 import jwt from "jsonwebtoken";
 
 import * as authServices from "../services/authServices.js";
-
+import path from "path";
 import HttpError from "../helpers/HttpError.js";
-
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import fs from "fs/promises";
 
 import dotenv from "dotenv";
+import { resizeImage } from "../middlewares/imageHelpers.js";
 dotenv.config();
 const { JWT_SECRET } = process.env;
 
+
+
 const register = async (req, res) => {
   const { email } = req.body;
+
   const user = await authServices.findUser({ email });
   if (user) {
     throw HttpError(409, "Email in use");
@@ -69,10 +73,35 @@ const logout = async (req, res) => {
     message: "Logout success",
   });
 };
+const updateAvatar = async (req, res) => {
+ 
+      const { path: oldPath, originalname } = req.file;
+      const { _id: userId } = req.user; 
+
+    
+      await resizeImage(oldPath);
+
+      
+      const newImageName = `${userId}_${originalname}`;
+
+      const newPath = path.join("public", "avatars", newImageName);
+
+     
+      await fs.rename(oldPath, newPath);
+
+
+      const result = await authServices.updateUser(userId, { avatarURL: newPath });
+
+      res.status(200).json({
+          avatarURL: result.avatarURL
+      });
+
+};
 
 export default {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
